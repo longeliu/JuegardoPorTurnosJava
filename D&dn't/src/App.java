5f34c4cf3d82f5e1e6d3817e2de8b27f84b821a6
drewdj <43813535+drewdj@users.noreply.github.com>
2021-05-17 11:19:29 +0200

    Desarrollo FactoryEnemigos

--- D&dn't/src/App.java
+++ D&dn't/src/App.java
@@ -1,5 +1,11 @@
+import Pruebas.CreacionPersonaje;
+import Character.Player;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Player personaje=new Player(10,10,10);
+        CreacionPersonaje prueba = new CreacionPersonaje();
+
+        prueba.creapersonaje(personaje);
+
     }
 }
--- D&dn't/src/Character/Player.java
+++ D&dn't/src/Character/Player.java
@@ -6,6 +6,30 @@ public class Player implements Character{
     private int fuerza;
     private int destreza;
     private int inteligencia;
+    private int vida;
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public Player(int fuerza, int destreza, int inteligencia){
+        this.fuerza=fuerza;
+        this.destreza=destreza;
+        this.inteligencia=inteligencia;
+        this.vida=10+((fuerza-10)/2);
+    }
 
     @Override
     public Ataque ataque1() {
